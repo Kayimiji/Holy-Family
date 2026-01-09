59235f6d38efe7c52f7db4c323de754e3872d5f5
Kcasandra01 <kristinacasandra294@gmail.com>
2026-01-09 11:45:59 +0800

    addstage

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.CONFIG = {
-  API_BASE_URL: "https://holyfamily-be.onrender.com", // production server
-  // API_BASE_URL: "http://localhost:3000", // for local development
+  //API_BASE_URL: "https://holyfamily-be.onrender.com", // production server
+  API_BASE_URL: "http://localhost:3000", // for local development
   APP_ENV: "production"
 };
